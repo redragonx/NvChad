9db5153ec2fbe5bf3ca8c2febeb1525f5fdac14c
Stephen Chavez <stephen.chavez12@gmail.com>
2023-08-10 21:52:29 -0600

    yay

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,92 @@
+local M = {}
+
+
+
+M.crates = {
+  plugin = true,
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require('crates').upgrade_all_crates()
+      end,
+      "update crates"
+    }
+  }
+}
+
+M.rusttools = {
+  n = {
+    ["<leader>rcc"] = {
+      function ()
+        require'rust-tools'.open_cargo_toml.open_cargo_toml()
+      end,
+      "Open Cargo.toml file"
+    },
+  },
+}
+
+M.extras = {
+  n = {
+    ["<leader>f"] = {
+      function ()
+      vim.lsp.buf.format()
+      end,
+      "Format buffer"
+    },
+  },
+}
+
+M.minimap = {
+  n = {
+    ["<leader>mm"] = {
+      function()
+        require("codewindow").toggle_minimap()
+      end,
+      " Toggle minimap",
+    },
+  },
+}
+
+M.harpoon = {
+  n = {
+    ["<leader>ha"] = {
+      function()
+        require("harpoon.mark").add_file()
+      end,
+      "󱡁 Harpoon Add file",
+    },
+    ["<leader>ta"] = { "<cmd>Telescope harpoon marks<CR>", "󱡀 Toggle quick menu" },
+    ["<leader>hb"] = {
+      function()
+        require("harpoon.ui").toggle_quick_menu()
+      end,
+      "󱠿 Harpoon Menu",
+    },
+    ["<leader>1"] = {
+      function()
+        require("harpoon.ui").nav_file(1)
+      end,
+      "󱪼 Navigate to file 1",
+    },
+    ["<leader>2"] = {
+      function()
+        require("harpoon.ui").nav_file(2)
+      end,
+      "󱪽 Navigate to file 2",
+    },
+    ["<leader>3"] = {
+      function()
+        require("harpoon.ui").nav_file(3)
+      end,
+      "󱪾 Navigate to file 3",
+    },
+    ["<leader>4"] = {
+      function()
+        require("harpoon.ui").nav_file(4)
+      end,
+      "󱪿 Navigate to file 4",
+    },
+  },
+}
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,98 @@
+local plugins = {
+-- In order to modify the `lspconfig` configuration:
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {"rust", },
+    },
+  },
+  {
+    "chikko80/error-lens.nvim",
+    ft = "rust",
+    dependencies = {
+        "nvim-telescope/telescope.nvim"
+    },
+  },
+ {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+        "rust-analyzer"
+      },
+    },
+    {
+      "simrat39/rust-tools.nvim",
+      ft = "rust",
+      dependencies = "neovim/nvim-lspconfig",
+      opts = function ()
+        return require "custom.configs.rust-tools"
+      end,
+      config = function(_, opts)
+        require('rust-tools').setup(opts)
+      end
+    },
+    {
+      'saecki/crates.nvim',
+      ft = {"rust", "toml"},
+      config = function(_, opts)
+        local crates  = require('crates')
+        crates.setup(opts)
+        require('cmp').setup.buffer({
+          sources = { { name = "crates" }}
+        })
+        crates.show()
+        require("core.utils").load_mappings("crates")
+      end,
+    },
+    {
+      "hrsh7th/nvim-cmp",
+      opts = function()
+        local M = require "plugins.configs.cmp"
+        local cmp = require('cmp')
+        M.completion.completeopt = "menu,menuone,noselect"
+        M.mapping["<CR>"] = cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = false,
+        }
+        table.insert(M.sources, {name = "crates"})
+        return M
+      end,
+    },
+    {
+      "ThePrimeagen/harpoon"
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+
+      opts = {
+        extensions_list = { "themes", "terms", "harpoon" },
+        extensions = {
+        },
+      },
+    },
+    {
+      "gorbit99/codewindow.nvim",
+      keys = { "<leader>mm" },
+      config = function()
+        require("codewindow").setup {
+          show_cursor = false,
+          window_border = "rounded",
+        }
+      end,
+    }, 
+  }
+
+}
+
+return plugins
